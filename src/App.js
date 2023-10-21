be7cb957a7f8a5e00c293e08ac7ce6b4c6114777
DiasGalimzhanov <126086058+DiasGalimzhanov@users.noreply.github.com>
2023-10-21 22:20:45 +0600

    Interstellar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import Interstellar from './Interstellar';
 
 function App() {
+
+  const filmInfo = {
+    title: "Интерстеллар",
+    director: "Кристофер Нолан",
+    releaseYear: 2014,
+    studio: "Paramount Pictures",
+    posterURL: "https://upload.wikimedia.org/wikipedia/ru/c/c3/Interstellar_2014.jpg",
+  };
+
   return (
     <div className="App">
-      <h1>Dias</h1>
-      <h2>Number: 8776*****55</h2>
-      <h3>dias@bruh.com</h3>     
+      <Interstellar
+        title={filmInfo.title}
+        director={filmInfo.director}
+        releaseYear={filmInfo.releaseYear}
+        studio={filmInfo.studio}
+        posterURL={filmInfo.posterURL}
+      />   
     </div>
   );
 }
