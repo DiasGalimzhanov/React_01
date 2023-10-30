337c2c22f149abfdcc1a2edad9c9b54fe76ece51
DiasGalimzhanov <126086058+DiasGalimzhanov@users.noreply.github.com>
2023-10-30 23:18:41 +0600

    hm_05 calculate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import './App.css';
-import Interstellar from './Interstellar';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState(''); 
+  
+  const updateInput = (value) => {
+    setInputValue((prevValue) => prevValue + value);
+  };
 
-  const filmInfo = {
-    title: "Интерстеллар",
-    director: "Кристофер Нолан",
-    releaseYear: 2014,
-    studio: "Paramount Pictures",
-    posterURL: "https://upload.wikimedia.org/wikipedia/ru/c/c3/Interstellar_2014.jpg",
+  const calculateResult = () => {
+    setInputValue(eval(inputValue).toString());
   };
 
   return (
     <div className="App">
-      <Interstellar
-        title={filmInfo.title}
-        director={filmInfo.director}
-        releaseYear={filmInfo.releaseYear}
-        studio={filmInfo.studio}
-        posterURL={filmInfo.posterURL}
-      />   
+      <input value={inputValue} readOnly />
+      <div>
+        <button onClick={() => updateInput('7')}>7</button>
+        <button onClick={() => updateInput('8')}>8</button>
+        <button onClick={() => updateInput('9')}>9</button>
+        <button onClick={() => updateInput('+')}>+</button>
+      </div>
+      <div>
+        <button onClick={() => updateInput('4')}>4</button>
+        <button onClick={() => updateInput('5')}>5</button>
+        <button onClick={() => updateInput('6')}>6</button>
+        <button onClick={() => updateInput('-')}>-</button>
+      </div>
+      <div>
+        <button onClick={() => updateInput('1')}>1</button>
+        <button onClick={() => updateInput('2')}>2</button>
+        <button onClick={() => updateInput('3')}>3</button>
+        <button onClick={() => updateInput('*')}>*</button>
+      </div>
+      <div>
+        <button onClick={() => updateInput('.')}>.</button>
+        <button onClick={() => updateInput('0')}>0</button>
+        <button onClick={calculateResult}>=</button>
+        <button onClick={() => updateInput('/')}>/</button>
+      </div>
     </div>
+
   );
 }
 
